aedd1444b9fb81f6303c3bb033735a8f8d7f50aa
johncooked <152047905+johncooked@users.noreply.github.com>
2024-12-18 17:12:01 +1000

    Update main.js

--- my-app/main.js
+++ my-app/main.js
@@ -31,7 +31,7 @@ app.whenReady().then(() => {
 });
 
 app.on("window-all-closed", () => {
-    if (process.platform != "darwin") {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
